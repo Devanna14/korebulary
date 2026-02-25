081eb8f292b7cc162a7f319f893de9277c1fdc32
Laraana <cecylar14@gmail.com>
2026-02-24 23:49:58 -0600

    Update: Nueva data

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,9 @@
 // Este archivo contiene datos de ejemplo para el vocabulario coreano.
 export const vocabularioCoreano = [
-  { id: 1, hangul: "안녕하세요", romanizacion: "Annyeong-haseyo", significado: "Hola" },
-  { id: 2, hangul: "감사합니다", romanizacion: "Gamsahamnida", significado: "Gracias" },
+  { id: 1, hangul: "안녕하세요", romanizacion: "Añyong-haseyo", significado: "Hola" },
+  { id: 2, hangul: "감사합니다", romanizacion: "Kamsahamnida", significado: "Gracias" },
   { id: 3, hangul: "물", romanizacion: "Mul", significado: "Agua" },
   { id: 4, hangul: "친구", romanizacion: "Chingu", significado: "Amigo/a" },
-  { id: 5, hangul: "사랑해", romanizacion: "Saranghae", significado: "Te amo" },
+  { id: 5, hangul: "사랑해", romanizacion: "Sarangne", significado: "Te amo" },
+  { id: 6, hangul: "바보", romanizacion: "Pavo", significado: "Tonto" },
 ];
