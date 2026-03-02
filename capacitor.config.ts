70b44b1a01a30a5386bb9ba07dcf8735cb5e1e26
Cecy <cecylar14@gmail.com>
2026-03-02 16:22:24 -0600

    Feature: Se agregan los recursos del icono y del splash screen para telefono

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.korebularyjoapps.app',
   appName: 'korebulary',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // 3 segundos de splash
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff", // Color de fondo en hex
+      androidScaleType: "CENTER_CROP"
+    },
+  },
 };
 
 export default config;
